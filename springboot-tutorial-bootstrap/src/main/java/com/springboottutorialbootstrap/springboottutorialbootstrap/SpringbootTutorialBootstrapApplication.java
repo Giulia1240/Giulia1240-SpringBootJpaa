e315971019d7ad01ac03764107f73c7ac549a60f
Giulia Forlin Cavalcanti <64926458+Giulia1240@users.noreply.github.com>
2023-02-28 20:13:00 -0300

    project created

---
+++ springboot-tutorial-bootstrap/src/main/java/com/springboottutorialbootstrap/springboottutorialbootstrap/SpringbootTutorialBootstrapApplication.java
@@ -0,0 +1,13 @@
+package com.springboottutorialbootstrap.springboottutorialbootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTutorialBootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTutorialBootstrapApplication.class, args);
+	}
+
+}
---
+++ springboot-tutorial-bootstrap/src/test/java/com/springboottutorialbootstrap/springboottutorialbootstrap/SpringbootTutorialBootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboottutorialbootstrap.springboottutorialbootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTutorialBootstrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
